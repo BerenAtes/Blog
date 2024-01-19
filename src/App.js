c72d2ee8a8fccafbe03f0fe9d5d22c2680bba59c
Beren Ateş <145322724+BerenAtes@users.noreply.github.com>
2024-01-19 20:58:14 +0300

    my portolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const images = [
+  "https://picsum.photos/200",
+  "https://picsum.photos/200",
+  "https://picsum.photos/200",
+  "https://picsum.photos/200",
+  "https://picsum.photos/200",
+  "https://picsum.photos/200",
+  "https://picsum.photos/200",
+  "https://picsum.photos/200",
+];
+
+const ProjectItem = ({ imgSrc, text }) => (
+  <div>
+    <img src={imgSrc} alt="Project" />
+    <p>{text}</p>
+  </div>
+);
+
+const App = () => (
+  <div>
+    <header className="header">
+      <h1>Beren ATEŞ</h1>
+      <nav className="navbar">
+        <a href="#">Anasayfa</a>
+        <a href="#">Hakkında</a>
+        <a href="#">İletişim</a>
+        <a href="#">Projeler</a>
+      </nav>
+    </header>
+
+    <section className="profile">
+      <div className="logo-text">
+        <img src={process.env.PUBLIC_URL + "/vesika.png"} alt="Beren ATEŞ" />
+        <div className="text">
+          <h1>Beren ATEŞ</h1>
+          <a href="#">Projelerim</a>
+          <div className="decoration">
+            <p>
+              Merhaba, ben Beren Ateş. 1999 yılında Bursa'nın Nilüfer ilçesinde
+              doğdum. Gebze Teknik Üniversitesi Harita Mühendisliği bölümünden
+              2023 yılında mezun oldum.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="box-container">
+        {images.map((imageSrc, index) => (
+          <ProjectItem key={index} imgSrc={imageSrc} text="YAZI" />
+        ))}
+      </div>
+    </section>
+
+    <footer>
+      <div className="text-container">
+        <h3>Gördüklerinden memnun musun?</h3>
+        <a href="#">Bana Ulaş</a>
+      </div>
+    </footer>
+  </div>
+);
 
 export default App;
